c97d1296781b71183c94e2bc118c4fe2468590c1
ablahum <mriztama@gmail.com>
2023-01-03 17:48:38 +0700

    refacto

---
+++ src/views/AblahumView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ablahum">
+    <h1>ini route untuk ablahum</h1>
+
+    <p>hahaha</p>
+
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AblahumView',
+  props: {
+    message: String,
+  },
+};
+</script>
