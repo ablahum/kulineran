765a81fb86910c9dcd78318edf882666ef07a3ff
ablahum <mriztama@gmail.com>
2023-01-06 11:04:16 +0700

    refacto

---
+++ src/components/HeroComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2>Makanan lezat, <br /><strong>dalam genggaman Anda.</strong></h2>
+
+          <p>Ayo segera pilih pesan makanan favorit Anda!</p>
+
+          <button class="btn btn-primary">pesan sekarang</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <img
+        src="../assets/hero.svg"
+        alt="eating together"
+        width="400"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroComp',
+};
+</script>
